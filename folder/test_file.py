ff105b2628daf25ab3ee55ccd2c9d152f25772fb
Saikarthik-0398 <97424664+Saikarthik-0398@users.noreply.github.com>
2024-09-05 09:37:28 +0530

    Add files via upload

---
+++ folder/test_file.py
@@ -0,0 +1,10 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.get("/")
+def fun():
+    name = request.args.get("name")
+    if name is None:
+        return "helloworld"
+    return "hello mr "+name+" nice to meet u"
+app.run()
